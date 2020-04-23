aaf5015e616edce217147caceb1763dda58af4b1
Lasse Kangas <46774902+t3kala01@users.noreply.github.com>
2020-04-23 15:43:46 +0300

    Working
    Gets gps location and prints long&lat

--- Lab1/app/src/main/java/com/example/gps_location/MainActivity.java
+++ Lab1/app/src/main/java/com/example/gps_location/MainActivity.java
@@ -1,24 +1,68 @@
 package com.example.gps_location;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
 
     TextView tvLocation;
     Button btnGps;
+    private double longitude, latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
         tvLocation = findViewById(R.id.textLocation);
         btnGps = findViewById(R.id.buttonLocation);
+
+        btnGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+
+                client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location != null){
+
+                            longitude = location.getLongitude();
+                            latitude = location.getLatitude();
+                            tvLocation.setText("Longitude:" + longitude +"\nLatitude:" + latitude);
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 
 
