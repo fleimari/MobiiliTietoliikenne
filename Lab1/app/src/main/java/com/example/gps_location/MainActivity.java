5ed74bb39f16a225d05b06789d517eeda1ac4e0d
Lasse Kangas <46774902+t3kala01@users.noreply.github.com>
2020-04-23 15:20:11 +0300

    First Commit
    Started

---
+++ Lab1/app/src/main/java/com/example/gps_location/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.gps_location;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvLocation;
+    Button btnGps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvLocation = findViewById(R.id.textLocation);
+        btnGps = findViewById(R.id.buttonLocation);
+    }
+
+
+}
